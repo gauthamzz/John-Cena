3fd7d6a8070d00027fe3caa825ab9ee6e5772543
Okegbile, Akin <akin.okegbile.contractor@bbva.com>
2018-02-17 23:56:15 -0600

    kotlin

---
+++ johncena.kt
@@ -0,0 +1,12 @@
+
+
+internal object johncena {
+    @JvmStatic
+    fun main(args: Array<String>) {
+        val output = ""
+
+        while (true) {
+            println(output)
+        }
+    }
+}
