69db597e0343c97e2aad95b5abe633a1f0cbe551
1-byte-man <56204784+1-byte-man@users.noreply.github.com>
2019-10-08 19:36:47 -0400

    another fix

--- john-Cena-linux-i386.asm
+++ john-Cena-linux-i386.asm
@@ -4,15 +4,15 @@ section .data
 	len	equ	$	-	msg
 
 section .text
-	global main
+	global _start
 
-john:
+_start:
 	mov	eax, 04h
 	mov	ebx, 01h
 	mov	ecx, johncena
 	mov	edx, len
 	int	80h
-	call	john
+	call	_start
 	mov	eax, 01h
 	mov	ebx, 00h
 	int	80h
